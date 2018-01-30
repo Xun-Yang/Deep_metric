aff215bfd9ad389488e36e8ba9b008f8ace8e596
bnuwangxun <wx119141@qq.com>
2018-01-30 15:59:33 +0800

    add DistweightLoss-

--- losses/__init__.py
+++ losses/__init__.py
@@ -6,6 +6,7 @@ from .BatchAll import BatchAllLoss
 from .NeighbourLoss import NeighbourLoss
 from .DistanceMatchLoss import DistanceMatchLoss
 from .NeighbourHardLoss import NeighbourHardLoss
+from .DistWeightLoss import DistWeightLoss
 
 __factory = {
     'triplet': TripletLoss,
@@ -15,6 +16,7 @@ __factory = {
     'neighbour': NeighbourLoss,
     'distance_match': DistanceMatchLoss,
     'neighard': NeighbourHardLoss,
+    'distweight': DistWeightLoss,
 }
 
 
