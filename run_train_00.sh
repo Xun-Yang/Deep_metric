35d5ee34986640866bf570e2bdce6678c649ab6f
bnuwangxun <wx119141@qq.com>
2018-02-03 16:13:06 +0800

    dist dev branch-

--- run_train_00.sh
+++ run_train_00.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=7 python train.py -data cub -net bn  -lr 1e-6 -dim 512   -num_instances 8 -BatchSize 128  -loss dwdevbranch  -epochs 401 -log_dir dwdevbranch  -save_step 50
+CUDA_VISIBLE_DEVICES=7 python train.py -data cub -net branch  -lr 1e-6 -dim 512   -num_instances 8 -BatchSize 128  -loss dwdevbranch  -epochs 401 -log_dir dwdevbranch  -save_step 50
 python test.py -r checkpoints/dwdevbranch/model.pkl
 python test.py -r checkpoints/dwdevbranch/50_model.pkl
 python test.py -r checkpoints/dwdevbranch/100_model.pkl
