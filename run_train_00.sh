eeb297b348439bb531fb100493b602e6a96e0b60
bnuwangxun <wx119141@qq.com>
2018-02-05 17:18:47 +0800

    ensemble DistWeight Neighbour Loss-

--- run_train_00.sh
+++ run_train_00.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=5 python train.py -data cub -net bn -base 0.1  -init rand   -s 270  -lr 1e-6 -dim 512   -num_instances 8 -BatchSize 128  -loss bdwneig_esemble  -epochs 701 -log_dir dw_neighbour  -save_step 50
+CUDA_VISIBLE_DEVICES=5 python train.py -data cub -net bn -base 0.1  -init rand   -s 270  -lr 1e-6 -dim 512   -num_instances 8 -BatchSize 128  -loss edwneig  -epochs 701 -log_dir edwneig  -save_step 50
 python test.py -r checkpoints/dw_neighbour/model.pkl >> result_bdwneig_esemble.txt
 python test.py -r checkpoints/dw_neighbour/50_model.pkl >> result_bdwneig_esemble.txt
 python test.py -r checkpoints/dw_neighbour/100_model.pkl >> result_bdwneig_esemble.txt
