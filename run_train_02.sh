0399244ec74c5b48e2aecf981eddf1cd647ca5a9
bnuwangxun <wx119141@qq.com>
2018-02-05 11:48:37 +0800

    branch neig -

--- run_train_02.sh
+++ run_train_02.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=5 python train.py -data cub -m 0.65 -net bn -base 0.1  -init rand  -s 250  -lr 1e-6 -dim 512   -num_instances 8 -BatchSize 128  -loss bdwneig  -epochs 601 -log_dir bdw_neighbour  -save_step 50
+CUDA_VISIBLE_DEVICES=6 python train.py -data cub -m 1 -net bn -base 0.1  -init rand  -s 200 -lr 1e-6 -dim 512   -num_instances 8 -BatchSize 128  -loss bdwneig  -epochs 601 -log_dir bdw_neighbour  -save_step 50
 python test.py -r checkpoints/bdw_neighbour/model.pkl >> result_bdw.txt
 python test.py -r checkpoints/bdw_neighbour/50_model.pkl >> result_bdw.txt
 python test.py -r checkpoints/bdw_neighbour/100_model.pkl >> result_bdw.txt
@@ -14,7 +14,7 @@ python test.py -r checkpoints/bdw_neighbour/600_model.pkl -test 0 >> result_bdw.
 
 
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=5 python train.py -data cub -m 0.65 -net bn -base 0.3  -init rand  -s 250  -lr 1e-6 -dim 512   -num_instances 8 -BatchSize 128  -loss bdwneig  -epochs 601 -log_dir bdw_neighbour  -save_step 50
+CUDA_VISIBLE_DEVICES=6 python train.py -data cub -m 1.0 -net bn -base 0.3  -init rand  -s 200 -lr 1e-6 -dim 512   -num_instances 8 -BatchSize 128  -loss bdwneig  -epochs 601 -log_dir bdw_neighbour  -save_step 50
 python test.py -r checkpoints/bdw_neighbour/model.pkl >> result_bdw.txt
 python test.py -r checkpoints/bdw_neighbour/50_model.pkl >> result_bdw.txt
 python test.py -r checkpoints/bdw_neighbour/100_model.pkl >> result_bdw.txt
@@ -29,7 +29,7 @@ python test.py -r checkpoints/bdw_neighbour/600_model.pkl -test 0 >> result_bdw.
 
 
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=5 python train.py -data cub -m 0.65 -net bn -base 0.5  -init rand  -s 250  -lr 1e-6 -dim 512   -num_instances 8 -BatchSize 128  -loss bdwneig  -epochs 601 -log_dir bdw_neighbour  -save_step 50
+CUDA_VISIBLE_DEVICES=6 python train.py -data cub -m 1.0 -net bn -base 0.5  -init rand  -s 200 -lr 1e-6 -dim 512   -num_instances 8 -BatchSize 128  -loss bdwneig  -epochs 601 -log_dir bdw_neighbour  -save_step 50
 python test.py -r checkpoints/bdw_neighbour/model.pkl >> result_bdw.txt
 python test.py -r checkpoints/bdw_neighbour/50_model.pkl >> result_bdw.txt
 python test.py -r checkpoints/bdw_neighbour/100_model.pkl >> result_bdw.txt
@@ -43,7 +43,7 @@ python test.py -r checkpoints/bdw_neighbour/600_model.pkl -test 0 >> result_bdw.
 
 
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=5 python train.py -data cub -m 0.65 -net bn -base 1  -init rand  -s 250  -lr 1e-6 -dim 512   -num_instances 8 -BatchSize 128  -loss bdwneig  -epochs 601 -log_dir bdw_neighbour  -save_step 50
+CUDA_VISIBLE_DEVICES=6 python train.py -data cub -m 1.0 -net bn -base 1  -init rand  -s 200 -lr 1e-6 -dim 512   -num_instances 8 -BatchSize 128  -loss bdwneig  -epochs 601 -log_dir bdw_neighbour  -save_step 50
 python test.py -r checkpoints/bdw_neighbour/model.pkl >> result_bdw.txt
 python test.py -r checkpoints/bdw_neighbour/50_model.pkl >> result_bdw.txt
 python test.py -r checkpoints/bdw_neighbour/100_model.pkl >> result_bdw.txt
@@ -58,7 +58,7 @@ python test.py -r checkpoints/bdw_neighbour/600_model.pkl -test 0 >> result_bdw.
 
 
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=5 python train.py -data cub -m 0.65 -net bn -base 5 -init rand  -s 250  -lr 1e-6 -dim 512   -num_instances 8 -BatchSize 128  -loss bdwneig  -epochs 601 -log_dir bdw_neighbour  -save_step 50
+CUDA_VISIBLE_DEVICES=6 python train.py -data cub -m 1.0 -net bn -base 5 -init rand  -s 200 -lr 1e-6 -dim 512   -num_instances 8 -BatchSize 128  -loss bdwneig  -epochs 601 -log_dir bdw_neighbour  -save_step 50
 python test.py -r checkpoints/bdw_neighbour/model.pkl >> result_bdw.txt
 python test.py -r checkpoints/bdw_neighbour/50_model.pkl >> result_bdw.txt
 python test.py -r checkpoints/bdw_neighbour/100_model.pkl >> result_bdw.txt
