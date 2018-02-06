f70ef297e07a88cce50f9dfca970d0bf1120430c
bnuwangxun <wx119141@qq.com>
2018-02-06 11:21:05 +0800

    adaptive margin neig--

--- run_train_01.sh
+++ run_train_01.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=4 python train.py -data cub -net bn -nums 0,52,102,152,204 -base 0.1  -init rand   -s 270  -lr 1e-6 -dim 512   -num_instances 8 -BatchSize 128  -loss dwneig  -epochs 701 -log_dir dwneig  -save_step 50
+CUDA_VISIBLE_DEVICES=4 python train.py -data cub -net bn  -base 0.1  -init rand   -s 200  -lr 1e-6 -dim 512   -num_instances 8 -BatchSize 128  -loss dwneig  -epochs 701 -log_dir dwneig  -save_step 50
 python test.py -r checkpoints/dwneig/model.pkl >> res_dwneig_adaptive_margin.txt
 python test.py -r checkpoints/dwneig/50_model.pkl >> res_dwneig_adaptive_margin.txt
 python test.py -r checkpoints/dwneig/100_model.pkl >> res_dwneig_adaptive_margin.txt
