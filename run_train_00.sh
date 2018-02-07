18b4d37ba22856ebfa62d63ff86cd3d4cf8e8c84
bnuwangxun <wx119141@qq.com>
2018-02-07 23:07:17 +0800

    soft-neig

--- run_train_00.sh
+++ run_train_00.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=3 python train.py -data cub -net bn  -base 0.1  -init rand   -s 200  -lr 1e-6 -dim 512   -num_instances 8 -BatchSize 128  -loss all_pos_28_neg_2  -epochs 601 -log_dir all_pos_28_neg_2  -save_step 50
+CUDA_VISIBLE_DEVICES=3 python train.py -data cub -net bn  -base 0.1  -init rand   -s 200  -lr 1e-6 -dim 512   -num_instances 8 -BatchSize 128  -loss softneig  -epochs 601 -log_dir all_pos_28_neg_2  -save_step 50
 python test.py -r checkpoints/all_pos_28_neg_2/model.pkl > all_pos_28_neg_2.txt
 python test.py -r checkpoints/all_pos_28_neg_2/50_model.pkl >> all_pos_28_neg_2.txt
 python test.py -r checkpoints/all_pos_28_neg_2/100_model.pkl >> all_pos_28_neg_2.txt
