7244b5834260488c11b128b2ae8caa589efee269
bnuwangxun <wx119141@qq.com>
2018-03-03 09:21:40 +0800

    clear 04

--- run_train_00.sh
+++ run_train_00.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=2 python train.py -data cub -net bn  -base 0.1  -init rand   -s 200  -lr 1e-6 -dim 512   -num_instances 8 -BatchSize 128  -loss bdwneig  -epochs 601 -log_dir neig  -save_step 50
-python test.py -r checkpoints/neig/model.pkl  >neig.txt
-python test.py -r checkpoints/neig/50_model.pkl  >>neig.txt
-python test.py -r checkpoints/neig/100_model.pkl  >>neig.txt
-python test.py -r checkpoints/neig/200_model.pkl  >>neig.txt
-python test.py -r checkpoints/neig/300_model.pkl  >>neig.txt
-python test.py -r checkpoints/neig/350_model.pkl  >>neig.txt
-python test.py -r checkpoints/neig/350_model.pkl -test 0 >>neig.txt
-python test.py -r checkpoints/neig/400_model.pkl  >>neig.txt
-python test.py -r checkpoints/neig/450_model.pkl  >>neig.txt
-python test.py -r checkpoints/neig/500_model.pkl  >>neig.txt
-python test.py -r checkpoints/neig/550_model.pkl  >>neig.txt
-python test.py -r checkpoints/neig/600_model.pkl  >>neig.txt
-python test.py -r checkpoints/neig/600_model.pkl -test 0 >>neig.txt
+CUDA_VISIBLE_DEVICES=2 python train.py -data cub -net bn  -base 0.1  -init rand   -s 200  -lr 1e-6 -dim 512   -num_instances 8 -BatchSize 128  -loss knnsoftmax  -epochs 601 -log_dir knnsoftmax  -save_step 50
+python test.py -r checkpoints/knnsoftmax/model.pkl  >knnsoftmax.txt
+python test.py -r checkpoints/knnsoftmax/50_model.pkl  >>knnsoftmax.txt
+python test.py -r checkpoints/knnsoftmax/100_model.pkl  >>knnsoftmax.txt
+python test.py -r checkpoints/knnsoftmax/200_model.pkl  >>knnsoftmax.txt
+python test.py -r checkpoints/knnsoftmax/300_model.pkl  >>knnsoftmax.txt
+python test.py -r checkpoints/knnsoftmax/350_model.pkl  >>knnsoftmax.txt
+python test.py -r checkpoints/knnsoftmax/350_model.pkl -test 0 >>knnsoftmax.txt
+python test.py -r checkpoints/knnsoftmax/400_model.pkl  >>knnsoftmax.txt
+python test.py -r checkpoints/knnsoftmax/450_model.pkl  >>knnsoftmax.txt
+python test.py -r checkpoints/knnsoftmax/500_model.pkl  >>knnsoftmax.txt
+python test.py -r checkpoints/knnsoftmax/550_model.pkl  >>knnsoftmax.txt
+python test.py -r checkpoints/knnsoftmax/600_model.pkl  >>knnsoftmax.txt
+python test.py -r checkpoints/knnsoftmax/600_model.pkl -test 0 >>knnsoftmax.txt
 
 
