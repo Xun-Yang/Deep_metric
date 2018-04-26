41b4e0bf31b6e171ba4cb068bee1c09190c6538a
bnu32 <bnuwangxun@gmail.com>
2018-04-26 14:19:33 +0800

    new start

---
+++ download.sh
@@ -0,0 +1,5 @@
+wget http://imagenet.stanford.edu/internal/car196/cars_annos.mat
+wget http://imagenet.stanford.edu/internal/car196/car_ims.tgz
+wget ftp://cs.stanford.edu/cs/cvgl/Stanford_Online_Products.zip
+wget http://www.vision.caltech.edu/visipedia-data/CUB-200/images.tgz
+wget http://data.lip6.fr/cadene/pretrainedmodels/bn_inception-239d2248.pth
