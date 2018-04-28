ad16c209173f7dce12721050ade88c028da14bb6
bnulihaixia <wx119141@qq.com>
2018-04-28 20:03:40 +0800

    bug -01

--- run_train_00.sh
+++ run_train_00.sh
@@ -3,7 +3,6 @@
 DATA="cub"
 loss="nca"
 checkpoints="/opt/intern/users/xunwang/checkpoints"
-l=$checkpoints/$loss/$DATA/$DIM
 r="_model.pkl"
 
 mkdir $checkpoints
@@ -16,11 +15,13 @@ mkdir result/$loss/$DATA/
 
 DIM_list="48 64 96 128 256 384 512 1024"
 for DIM in $DIM_list;do
-mkdir $checkpoints/$loss/$DATA/$DIM
-CUDA_VISIBLE_DEVICES=1 python train.py -data $DATA  -net bn  -init orth -lr 1e-5 -dim $DIM -alpha 16  -num_instances 8 -BatchSize 128 -loss $loss  -epochs 1001 -checkpoints $checkpoints -log_dir $loss/$DATA/$DIM  -save_step 100
-Model_LIST="100 200 300 400 500 600 700 800 900 1000"
+    l=$checkpoints/$loss/$DATA/$DIM
+    mkdir $checkpoints/$loss/$DATA/$DIM
+    CUDA_VISIBLE_DEVICES=1 python train.py -data $DATA  -net bn  -init orth -lr 1e-5 -dim $DIM -alpha 16  -num_instances 8 -BatchSize 128 -loss $loss  -epochs 1001 -checkpoints $checkpoints -log_dir $loss/$DATA/$DIM  -save_step 100
+    Model_LIST="100 200 300 400 500 600 700 800 900 1000"
     for i in $Model_LIST; do
         CUDA_VISIBLE_DEVICES=1  python test.py -data $DATA -r $l/$i$r >>result/$loss/$DATA/$DIM.txt
     done
 done
+
 
