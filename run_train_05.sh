e4a838539e9022f245cf987aca9ec49952dfd044
bnulihaixia <wx119141@qq.com>
2018-05-03 18:01:29 +0800

    modified cluster nca

--- run_train_05.sh
+++ run_train_05.sh
@@ -16,7 +16,7 @@ DIM_list="512 48 64 96 128 256 384 1024"
 for DIM in $DIM_list;do
     l=$checkpoints/$loss/$DATA/$DIM
     mkdir $checkpoints/$loss/$DATA/$DIM
-    CUDA_VISIBLE_DEVICES=7 python train.py -data $DATA  -net bn  -init orth -lr 1e-5 -dim $DIM -alpha 16 -beta 0.1 -n_cluster 25   -num_instances 8 -BatchSize 128 -loss $loss  -epochs 801 -checkpoints $checkpoints -log_dir $loss/$DATA/$DIM  -save_step 100
+    CUDA_VISIBLE_DEVICES=7 python train.py -data $DATA  -net bn  -init orth -lr 1e-5 -dim $DIM -alpha 16 -beta 0.1 -n_cluster 30   -num_instances 8 -BatchSize 128 -loss $loss  -epochs 801 -checkpoints $checkpoints -log_dir $loss/$DATA/$DIM  -save_step 100
     Model_LIST="100 200 300 400 500 600 700 800"
     for i in $Model_LIST; do
         CUDA_VISIBLE_DEVICES=7  python test.py -data $DATA -r $l/$i$r >>result/$loss/$DATA/$DIM.txt
