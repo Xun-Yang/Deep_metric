b27ee2e18639d0da91aef123c8d8e1568534de48
bnulihaixia <wx119141@qq.com>
2018-05-05 13:39:18 +0800

    cluster nca sh

--- run_train_06.sh
+++ run_train_06.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-DATA="cub"
+DATA="car"
 loss="cluster-nca"
 checkpoints="/opt/intern/users/xunwang/checkpoints"
 r="_model.pkl"
@@ -12,16 +12,16 @@ mkdir result/
 mkdir result/$loss/
 mkdir result/$loss/$DATA/
 
-DIM_list="512 64"
-Beta_list='0.05 0.15'
+DIM_list="512"
+Beta_list='0.05'
 for Beta in $Beta_list;do
 for Dim in $DIM_list;do
     l=$checkpoints/$loss/$DATA/$Beta-$Dim
     mkdir $checkpoints/$loss/$DATA/$Beta-$Dim
-    CUDA_VISIBLE_DEVICES=5 python train.py -data $DATA  -net bn  -init orth -lr 1e-5 -dim $Dim -alpha 20 -beta $Beta -n_cluster 30   -num_instances 8 -BatchSize 128 -loss $loss  -epochs 801 -checkpoints $checkpoints -log_dir $loss/$DATA/$Beta-$Dim  -save_step 100
+    CUDA_VISIBLE_DEVICES=6 python train.py -data $DATA  -net bn  -init orth -lr 1e-5 -dim $Dim -alpha 20 -beta $Beta -n_cluster 36  -num_instances 8 -BatchSize 128 -loss $loss  -epochs 801 -checkpoints $checkpoints -log_dir $loss/$DATA/$Beta-$Dim  -save_step 100
     Model_LIST="100 200 300 400 500 600 700 800"
     for i in $Model_LIST; do
-        CUDA_VISIBLE_DEVICES=5  python test.py -data $DATA -r $l/$i$r >>result/$loss/$DATA/$Beta-$Dim.txt
+        CUDA_VISIBLE_DEVICES=6  python test.py -data $DATA -r $l/$i$r >>result/$loss/$DATA/$Beta-$Dim.txt
     done
 done
 done
