6c25032a903e72538b34c2fdb8b18a0162e8b50e
bnulihaixia <wx119141@qq.com>
2018-05-09 17:34:41 +0800

    mca done

--- run_train_00.sh
+++ run_train_00.sh
@@ -16,9 +16,9 @@ DIM_list="64"
 for DIM in $DIM_list;do
     l=$checkpoints/$loss/$DATA/$DIM
     mkdir $checkpoints/$loss/$DATA/$DIM
-    CUDA_VISIBLE_DEVICES=1nvi    python MCA_train.py -data $DATA  -net bn  -init orth -lr 1e-5 -dim $DIM -alpha 16  -num_instances 8 -BatchSize 128 -loss $loss  -epochs 501 -checkpoints $checkpoints -log_dir $loss/$DATA/$DIM  -save_step 100
+    CUDA_VISIBLE_DEVICES=7   python MCA_train.py -data $DATA  -net bn  -init orth -lr 1e-5 -dim $DIM -alpha 16  -num_instances 8 -BatchSize 128 -loss $loss  -epochs 501 -checkpoints $checkpoints -log_dir $loss/$DATA/$DIM  -save_step 100
     Model_LIST="100 200 300 400 500"
     for i in $Model_LIST; do
-        CUDA_VISIBLE_DEVICES=1  python test.py -data $DATA -r $l/$i$r >>result/$loss/$DATA/$DIM-crop08.txt
+        CUDA_VISIBLE_DEVICES=7  python test.py -data $DATA -r $l/$i$r >>result/$loss/$DATA/$DIM-crop08.txt
     done
 done
